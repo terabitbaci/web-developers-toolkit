bf2f85164ef4be329f9966e44f865fb2c305c602
Marius Banea <marius@w-d-s.co.uk>
2016-07-24 04:11:53 +0100

    usage_of_equal_sign_in_programming_(comparisons)

---
+++ js/usage_of_equal_sign_in_programming_(comparisons).js
@@ -0,0 +1,21 @@
+/*
+The Usage of the Equal Sign in programming (Comparisons)
+
+// simple math
+1+2=3
+
+//change the boolean value of the variable to the opposite of it (if the hadouken was true is going to be false)
+hadouken = ! hadouken
+
+//non-strict comparisons (compare the variable only)
+//afirmative
+3 == "3" //true
+//negative
+3 != "3" //false
+
+//strict comparisons (compare the variable and the type of that variable)
+//afirmative
+3 === "3" //false
+//negative
+3 !== "3" //true
+*/
