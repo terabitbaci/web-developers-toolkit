d4daff2a247f2b8a4276478c9aae1d8384c2bca8
Marius Banea <marius@w-d-s.co.uk>
2016-10-07 21:27:08 +0100

    updating data types and github

--- js/variable-types.js
+++
@@ -1,6 +0,0 @@
-var numbers = 1; //integer
-var string = 'dog'; //string
-var boolean = true; //boolean
-var array = ['dog', 'cat', 'cow']; //array
-var object = {'bananas', 'oranges', 'apples'}; //object
-var functionName = function(parameter1, parameter2){}; //function
