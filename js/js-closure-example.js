549716978fbd11a405c489e413ee2e20945c456b
Marius Banea <marius@w-d-s.co.uk>
2017-01-25 02:41:27 +0000

    closure details

--- js/js-closure-example.js
+++ js/js-closure-example.js
@@ -1,10 +1,17 @@
-function showName (firstName, lastName) {
+//Definition:
+//A closure is a function inside another function inorder to protect the scope of the inner function
+
+//the outer (parent) function
+function showName(firstName, lastName) {
+
+    // nameIntro is global variable to the inner function but it is local to the outer function
     var nameIntro = "My name is ";
-    // this inner function has access to the outer function's variables, including the parameter
-    function makeFullName () {
+
+    // this inner (child) function has access to the outer (parent) function's variables, including the parameter
+    function makeFullName() {
         return nameIntro + firstName + " " + lastName;
     }
 
-    return makeFullName ();
+    return makeFullName();
 }
-showName ("James", "Bond"); // Your name is Michael Jackson
+showName("James", "Bond"); // My name is James Bond
