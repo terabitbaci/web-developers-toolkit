3e9170e0f9b4ae5e23d4354c0fac1bb1bb9e7540
Marius Banea <marius@w-d-s.co.uk>
2017-03-23 17:34:23 +0000

    updating data types, pantone colors and brackets 1.9

--- js/variable-data-types.js
+++ js/variable-data-types.js
@@ -21,11 +21,11 @@ computers[0] = "server";
 
 //arays with string as keys
 var car = ["make": "Nissan", "model": "Terrano", "engine": "3.5l"];
-car.make = “Nissan”;
+car.make = "Nissan";
 var carDetails = [
     "make": "Nissan",
     "model": "Terrano",
-    "engine": ["bhp": "160", "size": "3.5l", "turbo": true ]
+    "engine": ["bhp": "160", "size": "3.5l", "turbo": true]
 ];
 carDetails.engine.turbo = true
 
@@ -34,17 +34,20 @@ carDetails.engine.turbo = true
 
 //function definition used to create code templates https://github.com/mariusbanea/web-developers-toolkit/blob/master/js/functions-and-objects-theory-with-examples.js
 /*
+
+Definition: the functions are a set of instructions ready to be reused multiple times (is a like a recipe in a cook book)
+
  function components:
- -- function name (ex: "addingNumbers")
- -- input paramenters (ex: "parameter1", "parameter2")
- -- output paramenters (ex: "result")
- -- some set of instructions to convert the imput paramenters into output (ex: "var result = parameter1 + parameter2;")
+ -- function name (ex: "addingNumbers") (example: like a the recipe title)
+ -- input parameters (ex: "parameter1", "parameter2") (example: like a set of ingredients in a recipe)
+ -- output parameters (ex: "result") (example: like the description of what dish gets cooked from that recipe)
+ -- some set of instructions to convert the imput parameters into output (ex: "var result = parameter1 + parameter2;") (example: like the steps to cook the ingredients to create a dish in a recipe)
 */
 var addingNumbers = function (parameter1, parameter2) {
     var result = parameter1 + parameter2;
     return result;
 };
-//function usage / call
+//function usage / call (example: like cooking a recipe from the cook book)
 addingNumbers(3, 4); //result is 7
 addingNumbers(2, 9); //result is 11
 
