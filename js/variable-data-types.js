deea381ca86b4f4ffcfe1e1949d22766aaba5228
Marius Banea <marius@w-d-s.co.uk>
2017-03-23 17:47:23 +0000

    updating the data types

--- js/variable-data-types.js
+++ js/variable-data-types.js
@@ -34,20 +34,21 @@ carDetails.engine.turbo = true
 
 //function definition used to create code templates https://github.com/mariusbanea/web-developers-toolkit/blob/master/js/functions-and-objects-theory-with-examples.js
 /*
-
-Definition: the functions are a set of instructions ready to be reused multiple times (is a like a recipe in a cook book)
+The functions are a set of instructions ready to be reused multiple times (is a like a recipe in a cook book)
 
  function components:
  -- function name (ex: "addingNumbers") (example: like a the recipe title)
  -- input parameters (ex: "parameter1", "parameter2") (example: like a set of ingredients in a recipe)
  -- output parameters (ex: "result") (example: like the description of what dish gets cooked from that recipe)
  -- some set of instructions to convert the imput parameters into output (ex: "var result = parameter1 + parameter2;") (example: like the steps to cook the ingredients to create a dish in a recipe)
+
+function definition:
 */
 var addingNumbers = function (parameter1, parameter2) {
     var result = parameter1 + parameter2;
     return result;
 };
-//function usage / call (example: like cooking a recipe from the cook book)
+//function usage or call (example: like cooking a recipe from the cook book)
 addingNumbers(3, 4); //result is 7
 addingNumbers(2, 9); //result is 11
 
