bca1a64e05f1e7a994ce3ba2a7056ef4b0a294d9
Marius Banea <marius@w-d-s.co.uk>
2017-12-01 23:31:00 +0000

    variable data types updates and remote work

--- js/variable-data-types.js
+++ js/variable-data-types.js
@@ -12,21 +12,31 @@ var boolean = true;
 
 
 
-//arrays are key (index) and values pairs
+//arrays and objects have key (index) and values pairs
 
-//arays with integer as keys
+//arays have integer as keys
 var computers = ["server", "desktop", "tablet"];
+//is a shortcut for ..
 var computers = [0: "server", 1: "desktop", 2: "tablet"];
 computers[0] = "server";
 
-//arays with string as keys
-var car = ["make": "Nissan", "model": "Terrano", "engine": "3.5l"];
-car.make = "Nissan";
-var carDetails = [
+//objects have string as keys
+var car = {
     "make": "Nissan",
     "model": "Terrano",
-    "engine": ["bhp": "160", "size": "3.5l", "turbo": true]
-];
+    "engine": "3.5l"
+};
+
+car.make = "Nissan";
+var carDetails = {
+    "make": "Nissan",
+    "model": "Terrano",
+    "engine": {
+        "bhp": "160",
+        "size": "3.5l",
+        "turbo": true
+    }
+};
 carDetails.engine.turbo = true
 
 
@@ -51,13 +61,3 @@ var addingNumbers = function (parameter1, parameter2) {
 //function usage or call (example: like cooking a recipe from the cook book)
 addingNumbers(3, 4); //result is 7
 addingNumbers(2, 9); //result is 11
-
-
-
-
-//object
-var object = {
-    'bananas',
-    'oranges',
-    'apples'
-};
