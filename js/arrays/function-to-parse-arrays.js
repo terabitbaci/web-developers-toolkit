e71b398ef078f87cf8d5adbb172bb451a081116b
Marius Banea <marius@w-d-s.co.uk>
2019-01-20 03:11:55 +0000

    Update function-to-parse-arrays.js

--- js/arrays/function-to-parse-arrays.js
+++ js/arrays/function-to-parse-arrays.js
@@ -1,5 +1,5 @@
 // Original solutions by Teresa B. https://github.com/terabitbaci/
-const ARRAY = [
+let ARRAY = [
     "hockey",
     "lacrosse",
     "curling"
@@ -11,8 +11,8 @@ const ARRAY = [
 
 console.log('use a for loop to loop through the ARRAY and console the contents');
 
-for (let i = 0; i < ARRAY.length; i++) {
-    console.log(`fun sport ${i}: ${ARRAY[i]}`);
+for (let key = 0; key < ARRAY.length; key++) {
+    console.log(`fun sport ${key}: ${ARRAY[key]}`);
 }
 
 console.log('- - - - - - - - - -');
@@ -26,7 +26,7 @@ console.log('use a forEach to loop through the ARRAY and console the contents');
 // loop through the ARRAY and print the contents to the console using .forEach (https://www.w3schools.com/jsref/jsref_forEach.asp)
 // syntax: ARRAY.forEach((itemGoesHere, indexGoesHere)
 
-ARRAY.forEach((SPORT, NUM) => console.log(`fun sport ${NUM}: ${SPORT}`));
+ARRAY.forEach((value, key) => console.log(`fun sport ${value}: ${key}`));
 
 console.log('- - - - - - - - - -');
 
@@ -36,9 +36,9 @@ console.log('- - - - - - - - - -');
 
 console.log('use a Map to loop through the ARRAY and console the contents');
 
-function sports(item) {
+function sports(value, key) {
     // console.log('ES5 --> we are playing ' + item + '!');
-    console.log(`ES6 --> we are playing ${item} !`);
+    console.log(`ES6 --> we are playing ${key}: ${value} !`);
 };
 
 ARRAY.map(sports);
