3473255d9ae146f19d19e29054b5c51a2bb830c3
Marius Banea <marius@w-d-s.co.uk>
2019-02-24 23:26:27 +0000

    Update loops-basics.js

--- js/loops/loops-basics.js
+++ js/loops/loops-basics.js
@@ -1,36 +1,44 @@
 //use if you know both ends of the loop (START and STOP)
 //ex: count between START and STOP value using the STEP
-for (var counter = START; counter < END; counter + STEP) {
+for (let counter = START; counter < STOP; counter + STEP) {
     console.log("I am doing stuff");
 }
 
 //ascending for loop
-for (var i = 0; i <= 100; i + 3) {}
+for (let a = 0; a <= 100; a += 3) {
+    console.log(a);
+}
 
 //descending for loop
-for (var i = 5000; i >= 2000; i - 100) {}
+for (let b = 5000; b >= 2000; b -= 100) {
+    console.log(b);
+}
 
 
 
-//use if you know only one end of the loop (START or STOP)
-//ex: count up (down) to the START (STOP) value using the STEP
-while (var counter < LIMIT) {
+//use if you know only one end of the loop (START or STOP) so we will call it LIMIT
+//ex: count up (or down) to the LIMIT value using the STEP
+let counter = VALUE;
+while (counter < LIMIT) {
     console.log("I am doing stuff");
     //count towards the limit using some step value
     counter + STEP;
 }
 
 //ascending while loop going TOWARDS the limit (1000)
-while (var i < 1000) {
-    i++;
+let c = 0;
+while (c < 1000) {
+    c++;
 }
 
 //descending while loop going AWAY from the limit (5000)
-while (var i > 5000) {
-    i++;
+let d = 500;
+while (d > 5000) {
+    d++;
 } // ATTENTION: creates INFINITE LOOPS
 
 //descending while loop going TOWARDS from the limit (5000)
-while (var i > 5000) {
-    i--;
+let e = 10000;
+while (let e > 5000) {
+    e--;
 };
