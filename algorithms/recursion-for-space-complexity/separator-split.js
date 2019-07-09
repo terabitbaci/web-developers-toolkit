ebfa46484046e6312de6d6071a92242040f8f63d
Marius Banea <marius@w-d-s.co.uk>
2019-07-09 01:02:28 +0100

    adding factorial

--- algorithms/recursion-for-space-complexity/separator-split.js
+++ algorithms/recursion-for-space-complexity/separator-split.js
@@ -1,5 +1,6 @@
 'use strict';
-/* Exercise 5: Split a string based upon a separator (similar to String.prototype.split).
+/* Exercise 5:
+Write a recursive function that split a string based on a separator (similar to String.prototype.split).
 */
 
 let separator = (str, sep, arr, str2) => {
